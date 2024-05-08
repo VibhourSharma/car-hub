964d4fbc4d3f4d72e062724c54729f021c16af4a
Vibhour Sharma <vibhour121@gmail.com>
2024-05-08 07:33:13 +0530

    🚀feat: manufacturer, footer, Card, button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,44 @@
-import React from "react";
-import { Hero } from "../components/index";
+import { CarCard, CustomFilter, Hero, SearchBar } from "../components/index";
+import { fetchCars } from "../utils";
 
-const page = () => {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+
+        <div className="">
+          {!isDataEmpty ? (
+            <section>
+              <div className="home__cars-wrapper">
+                {allCars?.map((car) => (
+                  <CarCard car={car} />
+                ))}
+              </div>
+            </section>
+          ) : (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )}
+        </div>
+      </div>
     </main>
   );
-};
-
-export default page;
+}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
             className="object-contain"
           />
           <p className="text-base text-gray-700">
-            Carhub 2024 <br />
+            Carhub 2022 <br />
             All rights reserved &copy;
           </p>
         </div>
@@ -37,7 +37,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-        <p>@2023 CarHub. All rights reserved</p>
+        <p>@2022 CarHub. All rights reserved</p>
         <div className="footer__copyrights-link">
           <Link href="/" className="text-gray-500">
             Privacy Policy
